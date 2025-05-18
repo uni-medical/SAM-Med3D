7ee1ae5e402b7e96e6c9eade85aa45014ed33bc0
Haoyu Wang <small_dark@sina.com>
2025-05-18 22:14:23 +0800

    CHORE: local data url change

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 python train.py \
  --batch_size 2 \
- --task_name "ft_3DFM_b6x1" \
+ --num_workers 4 \
+ --task_name "ft_3DFM_b2x1" \
  --checkpoint "ckpt/sam_med3d_turbo_bbox_init.pth" \
- --lr 8e-5
+ --lr 8e-5 --port 12306
